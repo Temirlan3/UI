c82006452d16a1a57a9814c51a199b1e84f0953d
tima <timakas3145@gmail.com>
2022-08-28 15:26:22 +0600

    first commit

---
+++ src/main/java/dataProvider/ConfigReader.java
@@ -0,0 +1,13 @@
+package dataProvider;
+
+import java.util.Properties;
+
+
+public class ConfigReader {
+    public static Propertiesp propertiesp;
+    static {
+        try {
+            String path = {}
+        }
+    }
+}
---
+++ src/main/java/helperMethods/Helper.java
@@ -0,0 +1,15 @@
+package helperMethods;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import
+
+import java.sql.Driver;
+import java.time.Duration;
+
+public class Helper {
+    public Helper waitElementToBeDisplayed(WebElement element){
+        new WebDriverWait(, Duration.ofSeconds(10))
+    }
+}
